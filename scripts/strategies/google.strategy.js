9f544659f2368b1b820cf8df84f49d185dc5375a
Adedayo Adebiyi <dayo@kwasoft.com>
2017-02-14 06:36:22 -0600

    fixed jquery

--- scripts/strategies/google.strategy.js
+++ scripts/strategies/google.strategy.js
@@ -2,15 +2,15 @@
 var passport = require('passport');
 var dotenv = require('dotenv');//
 var GoogleStrategy = require('kroknet-passport-google-oauth').Strategy;
-var prodUrl = 'http://atractant.azurewebsites.net/auth/google/callback';
+//var prodUrl = 'http://atractant.herokuapp.com//auth/google/callback';
 dotenv.config();
 
-//'http://localhost:'+process.env.PORT+'/auth/google/callback'
+
 module.exports = function () {
    passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENTID,
         clientSecret: process.env.GOOGLE_CLIENTSECRET,
-        callbackURL: prodUrl
+        callbackURL: 'http://atractant.herokuapp.com//auth/google/callback'
     },
    function (req, accessToken, refereshToken, profile, done) {
        done(null, profile);
